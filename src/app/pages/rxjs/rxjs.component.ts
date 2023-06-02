c1fb3aef613d95522cd520d3538af2137c1a04c5
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-06-02 17:48:42 -0500

    crear un observable manualmente

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
-  styleUrls: ['./rxjs.component.css']
+  styleUrls: ['./rxjs.component.css'],
 })
 export class RxjsComponent {
+  constructor() {
+    const obs$ = new Observable((observer) => {
+      let i = -1;
+      const interval = setInterval(() => {
+        // console.log('tick');
+        i++;
+        observer.next(i);
 
+        if (i === 4) {
+          clearInterval(interval);
+          observer.complete();
+        }
+
+        if (i === 2) observer.error('i llego al valor de 2');
+      }, 1000);
+    });
+
+    obs$.subscribe(
+      (valor) => console.log('Subs', valor),
+      (err) => console.warn('Error', err),
+      () => console.info('Terminado')
+    );
+  }
 }
