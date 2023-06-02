2787075222f9c85da1a23a44d43d889e82bf19c9
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-06-02 10:55:55 -0500

    cambiar css de forma dinamica

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
-  styleUrls: ['./account-settings.component.css']
+  styleUrls: ['./account-settings.component.css'],
 })
 export class AccountSettingsComponent {
+  public linkTheme = document.querySelector('#theme');
 
+  changeTheme(theme: string) {
+    console.log(theme);
+    const url: string = '' + `./assets/css/colors/${theme}.css`;
+
+    this.linkTheme?.setAttribute('href', url);
+
+    localStorage.setItem('theme', url);
+  }
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
+  public linkTheme = document.querySelector('#theme');
 
+  ngOnInit(): void {
+    const url =
+      localStorage.getItem('theme') || './assets/css/colors/green-dark.css';
+    this.linkTheme?.setAttribute('href', url);
+  }
 }
