5fa6b7d0a5b196a50cba3b0d0e7cece7c6c6855d
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-06-03 01:15:31 -0500

    llamar el unsubscribe

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { Observable, filter, interval, map, retry, take } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import {
+  Observable,
+  Subscription,
+  filter,
+  interval,
+  map,
+  retry,
+  take,
+} from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css'],
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy {
+  public intervalSubs: Subscription;
   constructor() {
     // this.retornaObservable()
     //   .pipe(retry(2))
@@ -16,12 +25,17 @@ export class RxjsComponent {
     //     () => console.info('Terminado')
     //   );
 
-    this.retornaIntervalo().subscribe((valor) => console.log(valor));
+    this.intervalSubs = this.retornaIntervalo().subscribe((valor) =>
+      console.log(valor)
+    );
+  }
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe();
   }
 
   retornaIntervalo(): Observable<number> {
     return interval(100).pipe(
-      take(10),
+      // take(10),
       map((valor) => valor + 1),
       filter((valor) => (valor % 2 === 0 ? true : false))
     );
