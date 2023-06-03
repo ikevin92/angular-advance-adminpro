bc76f93a9d20980a4ec94a8cf337a36516eebcee
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-06-03 01:42:52 -0500

    optimizaciones breadcrumbs

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,32 +1,34 @@
-import { Component, inject, signal } from '@angular/core';
-import { ActivationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { Component, OnDestroy, inject, signal } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { Subscription, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styles: [],
 })
-export class BreadcrumbsComponent {
-  titulo = signal('');
+export class BreadcrumbsComponent implements OnDestroy {
+  public titulo = signal('');
+  public tituloSubs$?: Subscription;
   private router = inject(Router);
+  private route = inject(ActivatedRoute);
 
   constructor() {
-    this.getArgumentosRuta();
+    // console.log(this.route.snapshot.children[0].data);
+    this.tituloSubs$ = this.getArgumentosRuta().subscribe(({ titulo }) => {
+      this.titulo.set(titulo);
+      document.title = `AdminPro  ${this.titulo()}`;
+    });
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$?.unsubscribe();
   }
 
   getArgumentosRuta() {
-    this.router.events
-      .pipe(
-        filter(
-          (event): event is ActivationEnd => event instanceof ActivationEnd
-        ),
-        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
-        map((event: ActivationEnd) => event.snapshot.data)
-      )
-      .subscribe(({ titulo }) => {
-        this.titulo.set(titulo);
-        document.title = `AdminPro  ${this.titulo()}`;
-      });
+    return this.router.events.pipe(
+      filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
   }
 }
