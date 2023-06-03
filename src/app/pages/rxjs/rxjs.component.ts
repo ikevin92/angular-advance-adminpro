b3faed6743ab48566fda76f2e7ff7a0b13edcb69
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-06-03 01:10:35 -0500

    operador filter

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, interval, map, retry, take } from 'rxjs';
+import { Observable, filter, interval, map, retry, take } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -20,9 +20,10 @@ export class RxjsComponent {
   }
 
   retornaIntervalo(): Observable<number> {
-    return interval(1000).pipe(
-      take(4),
-      map((valor) => valor + 1)
+    return interval(100).pipe(
+      take(10),
+      map((valor) => valor + 1),
+      filter((valor) => (valor % 2 === 0 ? true : false))
     );
   }
 
